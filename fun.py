d12064c802436b30475449c4605afa0596a6bf4d
Jaydeep Sambangi <jcsambangi@gmail.com>
2018-09-11 15:40:15 -0400

    basic structure

---
+++ fun.py
@@ -0,0 +1,25 @@
+def main():
+    add_two(3, 5)
+    subtract_two(2, 1)
+    multiply_two(5, 5)
+
+def add_two(v1, v2):
+    p = v1 + v2
+    print(p)
+
+def subtract_two(v1, v2):
+    p = v1 - v2
+    print(p)
+
+def multiply_two(v1, v2):
+    """multiply two numbers
+
+    :param v1: number one
+    :param v2: number two
+    :returns:product number
+    """
+    p = v1 * v2
+    print(p)
+
+if _name_ == "_main_":
+    main()
